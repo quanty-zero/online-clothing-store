620f3f6c0b8ec36b589f51abd873dec9292c7d3f
Tomoe <4.artem.0@gmail.com>
2021-08-04 21:51:20 +0300

    Add bootstrap modules list

--- app/js/app.js
+++ app/js/app.js
@@ -1,3 +1,18 @@
+import 
+{ 
+	Alert,
+	Button,
+	Carousel,
+	Collapse,
+	Dropdown,
+	Modal,
+	Popover,
+	ScrollSpy,
+	Tab,
+	Toast,
+	Tooltip
+} from 'bootstrap';
+
 document.addEventListener('DOMContentLoaded', () => {
 
 	// Custom JS
