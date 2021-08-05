de5e2a31c52a6289c8272e58efeec172ca037154
Tomoe <4.artem.0@gmail.com>
2021-08-05 17:53:01 +0300

    Revert "Add gulp-dependents"
    This reverts commit 32b14dfca683ee686a5f51af92c5920f8f41d77c.

--- gulpfile.js
+++ gulpfile.js
@@ -19,54 +19,7 @@ const imagemin     = require('gulp-imagemin')
 const newer        = require('gulp-newer')
 const rsync        = require('gulp-rsync')
 const del          = require('del')
-const purgecss     = require('gulp-purgecss')
-const cached       = require('gulp-cached')
-const dependents   = require('gulp-dependents')
-var config = {
-    ".scss": {
- 
-        // The sequence of RegExps and/or functions to use when parsing
-        // dependency paths from a source file. Each RegExp must have the
-        // 'gm' modifier and at least one capture group. Each function must
-        // accept a string and return an array of captured strings. The
-        // strings captured by each RegExp or function will be passed
-        // to the next, thus iteratively reducing the file content to an
-        // array of dependency file paths.
-        parserSteps: [
- 
-            // PLEASE NOTE:
-            // The parser steps shown here are only meant as an example to
-            // illustrate the concept of the matching pipeline.
-            // The default config used for scss files is pure RegExp and
-            // reliably supports the full syntax of scss import statements.
- 
-            // Match the import statements and capture the text
-            // between '@import' and ';'.
-            /^\s*@import\s+(.+?);/gm,
- 
-            // Split the captured text on ',' to get each path.
-            function (text) { return text.split(","); },
- 
-            // Match the balanced quotes and capture only the file path.
-            /"([^"]+)"|'([^']+)'/m
-        ],
- 
-        // The file name prefixes to try when looking for dependency
-        // files, if the syntax does not require them to be specified in
-        // dependency statements. This could be e.g. '_', which is often
-        // used as a naming convention for mixin files.
-        prefixes: ['_'],
- 
-        // The file name postfixes to try when looking for dependency
-        // files, if the syntax does not require them to be specified in
-        // dependency statements. This could be e.g. file name extensions.
-        postfixes: ['.scss', '.sass'],
- 
-        // The additional base paths to try when looking for dependency
-        // files referenced using relative paths.
-        basePaths: [],
-    }
-};
+
 function browsersync() {
 	browserSync.init({
 		server: {
@@ -108,10 +61,10 @@ function scripts() {
 
 function styles() {
 	return src([`app/styles/${preprocessor}/*.*`, `!app/styles/${preprocessor}/_*.*`])
-        .pipe(cached('scssCache'))
-        .pipe(dependents(config))
 		.pipe(eval(`${preprocessor}glob`)())
 		.pipe(eval(preprocessor)())
+		.pipe(autoprefixer({ overrideBrowserslist: ['last 10 versions'], grid: true }))
+		.pipe(rename({ suffix: ".min" }))
 		.pipe(dest('app/css'))
 }
 
